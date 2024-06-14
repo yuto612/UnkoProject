1a4004dd29bd539773c6663bdc0094c8bb7153e6
saka <sakaguchi_y1@yahoo.co.jp>
2024-06-14 22:26:24 +0900

    ボールの反発を追加

--- Assets/Scripts/Ball/BallController.cs
+++ Assets/Scripts/Ball/BallController.cs
@@ -1,23 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace unkoProject
 {
     public class BallController : MonoBehaviour
     {
         [SerializeField] private Ball ballPrefab;
+        private Ball ball;
+        [SerializeField] private float minupforce=70;
+        [SerializeField] private float maxupforce=80;
 
         public Ball CreateBall()
         {
-            var prefab = Instantiate(ballPrefab);
-            prefab.OnCollision += OnCollision;
-            return prefab;
+            ball= Instantiate(ballPrefab);
+            ball.OnCollision += OnCollision;
+            return ball;
         }
 
         private void OnCollision(Collision collision)
         {
             Debug.Log(collision.gameObject.name);
+            var rand = Random.Range(minupforce, maxupforce);
+            Debug.Log(rand);
+            var force = new Vector3(0, rand, 0);
+            ball.GetComponent<Rigidbody>().AddForce(force);
         }
     }
 }
